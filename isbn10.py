b5dcfa4e77d4d56879b56d7d74473b15e73d70f6
Ellie <ellie.salimi@gmail.com>
2019-08-21 16:17:01 +0800

    isbn10 is working now

--- isbn10.py
+++ isbn10.py
@@ -12,21 +12,20 @@ def calculate_isbn10_barcode_check_digit(isbn_10):
     else:
         return(check_digit)
 
-
 def validate_isbn10_barcode_check_digit(isbn_10):
 
     sum = 0
-    isbn_new = list(isbn_10)
-    if isbn_new[9] == 'X':
-        isbn_new[9] = '0'
-    for position, digit in enumerate(isbn_new):
-        num_digit = int(digit)
-        # multiply first digit by 10, second by 9..etc
-        sum += (num_digit*(len(isbn_10)-position))
-    # formula to check if isbn10 is correct
+    for multiply_counter, num in enumerate(isbn_10,-10):
+        if num.isnumeric():
+            num = int(num)
+            sum += (num*(-multiply_counter))
+        else:
+            print ('This is an invalid number')
+    #check if isbn10 is correct
     if sum % 11 == 10 or sum % 11 == 0:
         return('This is a valid isbn10 barcode.')
     else:
         return('This is an invalid isbn10 barcode.')
 
-# print(validate_isbn10_barcode_check_digit('188879997X'))
+print(validate_isbn10_barcode_check_digit('1888799978'))
+print(validate_isbn10_barcode_check_digit('188879997X'))
